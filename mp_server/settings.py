01e3b4a2f1ddb3269c3329a148ca48b01f3929d0
Syed Hamadan Ahmad <f20212337@goa.bits-pilani.ac.in>
2025-01-29 11:17:19 +0530

    Update settings.py

--- mp_server/settings.py
+++ mp_server/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-#ba1b1e(%r-oirwm*)wc@70*#rk)elflo9pp71q+88uq^go811
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['3.108.31.127', '127.0.0.1]
+ALLOWED_HOSTS = ['3.108.31.127', '127.0.0.1']
 
 # Application definition
 
